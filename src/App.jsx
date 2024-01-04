ed27b3172d2ada25a91a451582ef72f6690339ac
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-03 23:33:31 -0600

    course 210

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route pathe="app" element={<AppLayout />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
       </Routes>
