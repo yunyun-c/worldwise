94094093a0d89a5d175207e27d42c391e7d75695
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-03 22:54:56 -0600

    course 209

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Router>
+      <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="product" element={<Product />}></Route>
         <Route path="pricing" element={<Pricing />}></Route>
+        <Route pathe="app" element={<AppLayout />}></Route>
         <Route path="*" element={<PageNotFound />}></Route>
-      </Router>
+      </Routes>
     </BrowserRouter>
   );
 }
