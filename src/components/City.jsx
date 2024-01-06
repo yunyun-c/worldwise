b3f26124713dd745e73a0df4171c1ab2ed7c00f4
cassie_c <cassie.nightingale.13.13@gmail.com>
2024-01-06 12:16:59 -0600

    course 217

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,6 +1,6 @@
 // import styles from "./City.module.css";
 
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 // const formatDate = (date) =>
 //   new Intl.DateTimeFormat("en", {
@@ -12,6 +12,11 @@ import { useParams } from "react-router-dom";
 
 function City() {
   const { id } = useParams();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
   // TEMP DATA
   const currentCity = {
     cityName: "Lisbon",
@@ -23,9 +28,15 @@ function City() {
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
-    <h1>
-      City {id} /{cityName}/ {emoji}/ {date}/ {notes}
-    </h1>
+    <>
+      <h1>
+        City {id} /{cityName}/ {emoji}/ {date}/ {notes}
+      </h1>
+
+      <p>
+        Position: {lat}, {lng}
+      </p>
+    </>
   );
 
   // return (
